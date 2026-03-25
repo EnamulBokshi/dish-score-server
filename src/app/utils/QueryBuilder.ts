93f23a307ae77be40595641bb2a2a300916cf4d4
EnamulBokshi <enamulbokshi@gmail.com>
2026-03-25 15:33:19 +0600

    feat: enhance data models with new relationships and fields for User, Dish, Restaurant, Review, Like, and Tag

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,14 +1,4 @@
-import {
-  IQueryConfig,
-  IQueryParams,
-  IQueryResult,
-  PrismaCountArgs,
-  PrismaFindManyArgs,
-  PrismaModelDelegate,
-  PrismaNumberFilter,
-  PrismaStringFilter,
-  PrismaWhereConditions,
-} from "../../interfaces/query.interface";
+
 
 // T = Model Type
 export class QueryBuilder<
